7073cc981a72254f16ea18f4195367bcfbd90784
John Hereshko <JayHereshko@fullsail.edu>
2014-01-23 21:50:45 -0500

    Finalized, added final claims to info

--- HereshkoJohn_assignment3.js
+++ HereshkoJohn_assignment3.js
@@ -69,6 +69,30 @@ var myPride = function(title){					// String Function, Argument: String
 	return title;
 };
 
+var infoCards = function(party){				
+	for (i = 0; i < party.possessors.length; i++){
+		var thisDemon = party.possessors[i];
+		console.log("Name: " + thisDemon.name + ", Element: " + thisDemon.element + ", Power: " + thisDemon.strength)
+	};
+	
+};
+
 
 
 var shallWeBegin = confirm("My name is Jay Valent, a half demon tasked with destroying the corrupted demons. I am on my way to attack a group now. Will you help me?");
+
+if (shallWeBegin === true){					// Conditional
+	var myGusto = confirm("There's an army of about 70 down there. Am I brave enough to fight them all...?");
+	thisBattle(myGusto);					// Nested Conditional
+}
+
+console.log("I have other demons at my disposal to help me fight the army!");
+
+myTeam(myDemons); 						// Pulling array info
+
+console.log("I have their information sheets in front of me")
+
+infoCards(party)						// Pulling JSON data
+
+console.log("However, their strength is not quite as strong as my own.");
+myPride(myTitle);						// Pulling String Data
