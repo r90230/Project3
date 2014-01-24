24dcf8c98306fd1d8dc415b2f38e2e07ce89fee7
John Hereshko <JayHereshko@fullsail.edu>
2014-01-23 19:53:16 -0500

    Added JSON database

--- HereshkoJohn_assignment3.js
+++ HereshkoJohn_assignment3.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+//John Hereshko
+// SDI 1401
+// Project 3
+// January 23, 2014
+
+var myHalfDemon = true,
+	myVolts = 10000,
+	myTitle = "Half Demon",
+	myDemons = ["Big Boss", "Ryzaragan", "Lucas", "Jono"];
+	
+var party = {
+        "possessors": [
+                {
+                "name": "Jay Valent"
+                "element": "lightning"
+                "strength": 942
+                 }
+                 {
+                "name": "Big Boss"
+                "element": "earth"
+                "strength": 890
+                }
+                {
+                "name": "Jono"
+                "element": "dark"
+                "strength": 850
+                }
+          }
+      ]
+}
